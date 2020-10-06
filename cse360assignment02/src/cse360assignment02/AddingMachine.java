1cf71e269d489453a4467e66102d2eece938b56d
jessicahuber-cs <jessica.q.huber@gmail.com>
2020-10-05 21:17:29 -0700

    Initial commit of the AddingMachine.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package cse360assignment02;
+
+/**
+ * This program performs novel math operations and prints.
+ * @author jessicahuber
+ * @date 10/05/2020
+ */
+public class AddingMachine {
+	private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * Returns the current value of the machine.
+	   * @return 	current value of type int
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * Adds the parameter to the total.
+	   * @param value	the value to be added to the total
+	   */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * Subtracts the parameter from the total.
+	   * @param value	the value to be subtracted from the total
+	   */
+	  public void subtract (int value) {
+	  }
+	  
+	  /**
+	   * Returns the history of the transactions of the machine as a formatted string.
+	   * @return	the history of the transactions as a string
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * Clears the "memory"
+	   */
+	  public void clear() {
+	  }
+}
